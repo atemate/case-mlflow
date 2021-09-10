07c8fea1e9326f50aecf2f52229381a107297dbb
Artem Yushkovsky <ajuszkowski@ya.ru>
2021-09-10 19:04:26 +0300

    Extract functions for iris (#2)

--- iris-sklearn/src/promote-model.py
+++ iris-sklearn/src/promote-model.py
@@ -1,19 +1,24 @@
-from mlflow.tracking import MlflowClient
 import os
 
-model_name = os.getenv("MY_MODEL_NAME", "iris")
-model_stage = os.getenv("MY_MODEL_STAGE", "Staging")
+from mlflow.tracking import MlflowClient
 
 
-client = MlflowClient()
-registered_model_info_list = client.list_registered_models()
-registered_model_info = registered_model_info_list[-1]
+def get_last_model_version(mlflow_client, model_name) -> int:
+    info_list = mlflow_client.search_model_versions(f"name='{model_name}'")
+    version = info_list[-1].version
+    return version
 
-version = registered_model_info.latest_versions[-1].version
 
-client.transition_model_version_stage(
-    model_name,
-    version=version,
-    stage=model_stage,
-)
-print(f"Transitioned model {model_name} version {version} to {model_stage}")
+if __name__ == "__main__":
+    model_name = os.getenv("MY_MODEL_NAME", "iris")
+    model_stage = os.getenv("MY_MODEL_STAGE", "Staging")
+
+    mlflow_client = MlflowClient()
+    latest_version = get_last_model_version(mlflow_client, model_name)
+
+    mlflow_client.transition_model_version_stage(
+        model_name,
+        version=latest_version,
+        stage=model_stage,
+    )
+    print(f"Transitioned model {model_name} version {latest_version} to {model_stage}")
